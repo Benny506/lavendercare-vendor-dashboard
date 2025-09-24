f632950dbfd6f3fa5299cbb29a8a5a753ad9c39a
benny506 <olomufeh@gmail.com>
2025-09-24 12:43:38 +0100

    updates

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/lavendercare-vendor-dashboard/',
+  base: '/vendor.lavendercare.co/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
